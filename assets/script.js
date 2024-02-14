f8fc502b812bc7d70588f7353e72ced5298f7889
BegunKetchup231 <142554725+BegunKetchup231@users.noreply.github.com>
2024-02-14 08:22:50 -0300

    Adição da pagina de suporte / style.css, e script.js

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+function downloadApp() {
+    var downloadLink = 'https://github.com/BegunKetchup231/Organizador-De-Arquivos/releases/tag/Alfa';
+    window.open(downloadLink, '_blank');
+}
+
+function navigateHome() {
+    window.location.href = '/index.html';
+}
+
+
+function openSupport() {
+    window.location.href = '/suporte.html';
+}
