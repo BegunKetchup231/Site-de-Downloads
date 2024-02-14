00a568631b7f1af71f796372075f8ac9f8c98313
BegunKetchup231 <142554725+BegunKetchup231@users.noreply.github.com>
2024-02-14 09:18:31 -0300

    Subir site pro gitsites

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 function downloadApp() {
-    var downloadLink = 'https://github.com/BegunKetchup231/Organizador-De-Arquivos/releases/tag/Alfa';
+    var downloadLink = 'https://github.com/BegunKetchup231/Organizador-De-Arquivos/releases/download/Alfa/OrganizadorDeArquivos-Release14-02-2024.zip';
     window.open(downloadLink, '_blank');
 }
 
@@ -7,7 +7,16 @@ function navigateHome() {
     window.location.href = '/index.html';
 }
 
-
 function openSupport() {
     window.location.href = '/suporte.html';
+}
+
+function openGitHub() {
+    var linkgithub = 'https://github.com/BegunKetchup231';
+    window.open(linkgithub, '_blank');
+}
+
+function moreinfo() {
+    var linkmoreinfogit = 'https://github.com/BegunKetchup231/Organizador-De-Arquivos/releases/tag/Alfa'
+    window.open(linkmoreinfogit, '_blank')
 }
