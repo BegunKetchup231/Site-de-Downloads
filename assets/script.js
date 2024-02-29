494de69f95ab7b7c24366470469cba2547c735c3
BegunKetchup231 <142554725+BegunKetchup231@users.noreply.github.com>
2024-02-29 14:17:35 -0300

    update

--- assets/script.js
+++ assets/script.js
@@ -3,20 +3,8 @@ function downloadApp() {
     window.open(downloadLink, '_blank');
 }
 
-function navigateHome() {
-    window.location.href = '/index.html';
-}
-
-function openSupport() {
-    window.location.href = '/suporte.html';
-}
-
 function openGitHub() {
-    var linkgithub = 'https://github.com/BegunKetchup231';
+    var linkgithub = 'https://github.com/BegunKetchup231/Organizador-de-Arquivos';
     window.open(linkgithub, '_blank');
 }
 
-function moreinfo() {
-    var linkmoreinfogit = 'https://github.com/BegunKetchup231/Organizador-De-Arquivos/releases/tag/Alfa'
-    window.open(linkmoreinfogit, '_blank')
-}
